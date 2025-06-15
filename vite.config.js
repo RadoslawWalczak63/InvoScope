81b58f1513dbe5646a564f6e2027104245916952
Radosław Walczak <radoslaw.walczak@netblink.net>
2025-06-15 08:57:43 +0200

    Setup Vite

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,36 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.ts',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
+export default ({ mode }) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+    return defineConfig({
+        server: {
+            host: true,
+            port: 5173,
+            https: false,
+            cors: {
+                origin: '*',
             },
-        }),
-    ],
-});
+            hmr: {
+                host: process.env.VITE_HMR_HOST,
+            },
+            allowedHosts: true,
+        },
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
+                },
+            }),
+        ],
+    });
+};
