851032a0afe44a4bc6b4c3e188e65d5a79651f8a
Radosław Walczak <radoslaw.walczak@netblink.net>
2026-02-01 21:01:00 +0100

    Fix HTTPS in axios

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -1,4 +1,9 @@
 import axios from 'axios';
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+const apiUrl =
+    import.meta.env.VITE_APP_URL_PRODUCT || 'https://invoscope.tech/';
+window.axios.defaults.baseURL = apiUrl;
