7518582ccb24e76c644efc5df9279133183d0ec3
Radosław Walczak <radoslaw.walczak@netblink.net>
2026-02-01 21:04:39 +0100

    Cleanup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
-use URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
         Vite::prefetch(concurrency: 3);
 
         if (env('APP_FORCE_HTTPS', false)) {
+            $this->app['request']->server->set('HTTPS', 'on');
             URL::forceScheme('https');
         }
     }
